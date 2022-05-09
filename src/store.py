4d77e96f3b3fd49a42811d37c0c1252e15f9533b
PL Sergent <papa.louis59@gmail.com>
2022-05-09 15:41:05 +0200

    Update store.py

--- src/store.py
+++ src/store.py
@@ -36,52 +36,52 @@ def store_profile():
 
 
 async def get_skins_from_api():
-    riot_session = aiohttp.ClientSession()
-    user_agent = "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
-    data = {
-        'client_id': 'play-valorant-web-prod',
-        'nonce': '1',
-        'redirect_uri': 'https://playvalorant.com/opt_in',
-        'response_type': 'token id_token',
-    }
-
-    headers = {'Content-Type': 'application/json', 'User-Agent': user_agent }
-
-    await riot_session.post('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
-
-    data = {
-        'type': 'auth',
-        'username': session.get('RIOT_USERNAME'),
-        'password': session.get('RIOT_PASSWORD')
-    }
-
-    async with riot_session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers) as r:
-        data = await r.json()
-
-    # Handle user authentication failure
-    if "error" in data.keys():
-        await riot_session.close()
-        return data["error"]
-    
-    pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
-    data = pattern.findall(data['response']['parameters']['uri'])[0]
-    access_token = data[0]
-
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-    async with riot_session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={}) as r:
-        data = await r.json()
-    entitlements_token = data['entitlements_token']
-
-    headers['X-Riot-Entitlements-JWT'] = entitlements_token
-
-    async with riot_session.post('https://auth.riotgames.com/userinfo', headers=headers, json={}) as r:
-        data = await r.json()
-    user_id = data['sub']
-    
-    # Request
     try:
+        riot_session = aiohttp.ClientSession()
+        user_agent = "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
+        data = {
+            'client_id': 'play-valorant-web-prod',
+            'nonce': '1',
+            'redirect_uri': 'https://playvalorant.com/opt_in',
+            'response_type': 'token id_token',
+        }
+
+        headers = {'Content-Type': 'application/json', 'User-Agent': user_agent }
+
+        await riot_session.post('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+
+        data = {
+            'type': 'auth',
+            'username': session.get('RIOT_USERNAME'),
+            'password': session.get('RIOT_PASSWORD')
+        }
+
+        async with riot_session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers) as r:
+            data = await r.json()
+
+        # Handle user authentication failure
+        if "error" in data.keys():
+            await riot_session.close()
+            return data["error"]
+
+        pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+        data = pattern.findall(data['response']['parameters']['uri'])[0]
+        access_token = data[0]
+
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+        async with riot_session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={}) as r:
+            data = await r.json()
+        entitlements_token = data['entitlements_token']
+
+        headers['X-Riot-Entitlements-JWT'] = entitlements_token
+
+        async with riot_session.post('https://auth.riotgames.com/userinfo', headers=headers, json={}) as r:
+            data = await r.json()
+        user_id = data['sub']
+    
+        # Request
         async with riot_session.get(f'https://pd.EU.a.pvp.net/store/v2/storefront/{user_id}', headers=headers) as r:
             data = json.loads(await r.text())
 
