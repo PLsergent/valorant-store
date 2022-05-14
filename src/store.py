624170846267a0a1c327d48ab7a8541f604c65f5
Pierre-Louis Sergent <papa.louis59@gmail.com>
2022-05-15 00:48:48 +0200

    Add skins price, and move functions to utils.py

--- src/store.py
+++ src/store.py
@@ -1,11 +1,11 @@
-import re
-import aiohttp
 import asyncio
-import json
 
 from flask import (
     Blueprint, flash, redirect, render_template, request, session, url_for
 )
+
+from . import utils
+
 
 bp = Blueprint('store', __name__, url_prefix='/')
 
@@ -22,7 +22,7 @@ def store_homepage():
 
 @bp.route("/store", methods=("GET", "POST"))
 def store_profile():
-    skins_data = asyncio.run(get_skins_from_api())
+    skins_data = asyncio.run(utils.get_skins_from_api())
 
     if skins_data == "auth_failure":
         flash("Authentication failed. Please try again.")
@@ -33,69 +33,3 @@ def store_profile():
         return redirect(url_for("store.store_homepage"))
         
     return render_template('store.html', data=skins_data)
-
-
-async def get_skins_from_api():
-    try:
-        riot_session = aiohttp.ClientSession()
-        user_agent = "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
-        data = {
-            'client_id': 'play-valorant-web-prod',
-            'nonce': '1',
-            'redirect_uri': 'https://playvalorant.com/opt_in',
-            'response_type': 'token id_token',
-        }
-
-        headers = {'Content-Type': 'application/json', 'User-Agent': user_agent }
-
-        await riot_session.post('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
-
-        data = {
-            'type': 'auth',
-            'username': session.get('RIOT_USERNAME'),
-            'password': session.get('RIOT_PASSWORD')
-        }
-
-        async with riot_session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers) as r:
-            data = await r.json()
-
-        # Handle user authentication failure
-        if "error" in data.keys():
-            await riot_session.close()
-            return data["error"]
-
-        pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
-        data = pattern.findall(data['response']['parameters']['uri'])[0]
-        access_token = data[0]
-
-        headers = {
-            'Authorization': f'Bearer {access_token}',
-        }
-        async with riot_session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={}) as r:
-            data = await r.json()
-        entitlements_token = data['entitlements_token']
-
-        headers['X-Riot-Entitlements-JWT'] = entitlements_token
-
-        async with riot_session.post('https://auth.riotgames.com/userinfo', headers=headers, json={}) as r:
-            data = await r.json()
-        user_id = data['sub']
-    
-        # Request
-        async with riot_session.get(f'https://pd.EU.a.pvp.net/store/v2/storefront/{user_id}', headers=headers) as r:
-            data = json.loads(await r.text())
-
-        data_store = data["SkinsPanelLayout"]["SingleItemOffers"]
-
-        skins_data = []
-
-        for skin in data_store:
-            async with riot_session.get(f"https://valorant-api.com/v1/weapons/skinlevels/{skin}") as r:
-                skins_data.append(json.loads(await r.text())["data"])
-    except Exception as e:
-        print(e)
-        return "error"
-
-    await riot_session.close()
-
-    return skins_data
---
+++ src/utils.py
@@ -0,0 +1,85 @@
+import aiohttp
+import json
+import re
+
+from flask import session
+
+
+async def get_skins_from_api():
+    try:
+        riot_session = aiohttp.ClientSession()
+        user_agent = "RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)"
+        data = {
+            'client_id': 'play-valorant-web-prod',
+            'nonce': '1',
+            'redirect_uri': 'https://playvalorant.com/opt_in',
+            'response_type': 'token id_token',
+        }
+
+        headers = {'Content-Type': 'application/json', 'User-Agent': user_agent }
+
+        await riot_session.post('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+
+        data = {
+            'type': 'auth',
+            'username': session.get('RIOT_USERNAME'),
+            'password': session.get('RIOT_PASSWORD')
+        }
+
+        async with riot_session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers) as r:
+            data = await r.json()
+
+        # Handle user authentication failure
+        if "error" in data.keys():
+            await riot_session.close()
+            return data["error"]
+        
+        pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+        data = pattern.findall(data['response']['parameters']['uri'])[0]
+        access_token = data[0]
+
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+        async with riot_session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={}) as r:
+            data = await r.json()
+        entitlements_token = data['entitlements_token']
+
+        headers['X-Riot-Entitlements-JWT'] = entitlements_token
+
+        async with riot_session.post('https://auth.riotgames.com/userinfo', headers=headers, json={}) as r:
+            data = await r.json()
+        user_id = data['sub']
+    
+        # Request
+        async with riot_session.get(f'https://pd.EU.a.pvp.net/store/v2/storefront/{user_id}', headers=headers) as r:
+            data = json.loads(await r.text())
+
+        data_store = data["SkinsPanelLayout"]["SingleItemOffers"]
+
+        skins_data = []
+
+        for skin in data_store:
+            async with riot_session.get(f"https://valorant-api.com/v1/weapons/skinlevels/{skin}") as r:
+                skin = json.loads(await r.text())["data"]
+            
+            async with riot_session.get(f'https://pd.EU.a.pvp.net/store/v1/offers/', headers=headers) as r:
+                offers = json.loads(await r.text())["Offers"]
+            
+            skin["price"] = get_skin_price_from_json(offers, skin["uuid"])
+            skins_data.append(skin)
+        
+    except Exception as e:
+        print(e)
+        await riot_session.close()
+        return "error"
+
+    await riot_session.close()
+
+    return skins_data
+
+
+def get_skin_price_from_json(offers, skin_id):
+    for item in offers:
+        if item["OfferID"] == skin_id:
+            return item["Cost"]["85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741"]
